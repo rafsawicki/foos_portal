33a2f45d7896bf58ae71de5deb496451a767b210
Rafal Sawicki <rafsawicki@users.noreply.github.com>
2016-09-09 00:16:46 +0200

    Add handling of + and -

--- app/controllers/slack_coordinator_controller.rb
+++ app/controllers/slack_coordinator_controller.rb
@@ -32,7 +32,7 @@ class SlackCoordinatorController < ApplicationController
         if game.in_progress?
           json_result[:text] = "Game currently in progress (#{list_players_in_game(game)})."
         else
-          json_result[:text] = "A game has already been started. Need #{game.players_needed_to_start} more #{pluralize_players(game.players_needed_to_start)} to start. Type _.in_ to join."
+          json_result[:text] = "A game has already been started. Need #{game.players_needed_to_start} more #{pluralize_players(game.players_needed_to_start)} to start. Type _.in_ or _+_ to join."
         end
       end
 
@@ -52,26 +52,47 @@ class SlackCoordinatorController < ApplicationController
         json_result[:text] = "#{@user} has already joined the game being set up."
       end
 
+    when '+'
+      begin
+        game = GameCreationService.create(@user)
+        json_result[:text] = "#{@user} is starting a new game. Need #{game.players_needed_to_start} more #{pluralize_players(game.players_needed_to_start)} to start."
+      rescue GameCreationService::GameInProgressError, GameCreationService::GameInSetupError => e
+        begin
+          game = GameJoiningService.join(@user)
+          if game
+            if game.in_progress?
+              json_result[:text] = "#{@user} has joined the game.\nTTT\n#{game.suggested_matchup}"
+            else
+              json_result[:text] = "#{@user} has successfully joined the game. Need #{game.players_needed_to_start} more #{pluralize_players(game.players_needed_to_start)} to start."
+            end
+          else
+            json_result[:text] = 'No game could be joined.'
+          end
+        rescue GameJoiningService::UserAlreadyJoinedError
+          json_result[:text] = "#{@user} has already joined the game being set up."
+        end
+      end
+    
     when 'quit'
       success = GameAbandoningService.abandon
       if success
-        json_result[:text] = 'Game has been abandoned. Type _.foos_ to start a new one.'
+        json_result[:text] = 'Game has been abandoned. Type _.foos_ or _+_ to start a new one.'
       else
-        json_result[:text] = 'There is no game to abandon. Type _.foos_ to start a new one.'
+        json_result[:text] = 'There is no game to abandon. Type _.foos_ or _+_ to start a new one.'
       end
 
-    when 'out'
+    when 'out', '-'
       begin
         game = GameQuittingService.quit(@user)
         if game
           json_result[:text] = "#{@user} has quit the current game. Need #{game.players_needed_to_start} more #{pluralize_players(game.players_needed_to_start)} to start."
         else
-          json_result[:text] = 'All players have quit the game. Type _.foos_ to start a new one.'
+          json_result[:text] = 'All players have quit the game. Type _.foos_ or _+_ to start a new one.'
         end
       rescue GameQuittingService::UserNotInGameError
         json_result[:text] = "#{@user} is not currently in a game."
       rescue GameQuittingService::GameNotFoundError
-        json_result[:text] = 'There is no game to quit. Type _.foos_ to start a new one.'
+        json_result[:text] = 'There is no game to quit. Type _.foos_ or _+_ to start a new one.'
       rescue GameQuittingService::WinResultFoundError
         json_result[:text] = 'There is already one win recorded. Type _.quit_ to abandon the game. Note: this will cause the current win to be lost.'
       end
@@ -140,7 +161,7 @@ class SlackCoordinatorController < ApplicationController
   end
 
   def command
-    params[:trigger_word]&.sub('.', '')
+    params[:trigger_word].sub('.', '')
   end
 
   def pluralize_players(count)
